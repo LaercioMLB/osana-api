79c51c6a6e2aacc7aa9aa64d832f3f484ed39a84
willian <willianthiagofozz@hotmail.com>
2022-08-15 22:25:27 -0300

    🎉 Commit inicial

---
+++ src/main/java/br/com/uniamerica/Osana/OsanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.Osana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uniamerica/Osana/OsanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.Osana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
