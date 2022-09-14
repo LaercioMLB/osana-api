9e23fbb1c196f8ca09b63d4022bcfab7af566777
Bruno Douglas <douglasbruno300@gmail.com>
2022-09-13 21:20:38 -0300

    swagger adicionado

--- src/main/java/br/com/uniamerica/Osana/Config/ErrorValidationHandler.java
+++ src/main/java/br/com/uniamerica/Osana/Config/ErrorValidationHandler.java
@@ -1,24 +1,64 @@
 package br.com.uniamerica.Osana.Config;
 
-import br.com.uniamerica.Osana.DTO.UserDTOS.ErrorFormDTO;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
-import java.util.List;
+import br.com.uniamerica.Osana.DTO.UserDTOS.ErrorFormDTO;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 @RestControllerAdvice
 public class ErrorValidationHandler {
+	
     @Autowired
     private MessageSource messageSource;
 
+	@SuppressWarnings("serial")
+	private List<ResponseMessage> responseMessageForGET()
+	{
+	    return new ArrayList<ResponseMessage>() {{
+	        add(new ResponseMessageBuilder()
+	            .code(500)
+	            .message("500 message")
+	            .responseModel(new ModelRef("Error"))
+	            .build());
+	        add(new ResponseMessageBuilder()
+	            .code(403)
+	            .message("Forbidden!")
+	            .build());
+	    }};
+	}
+	
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	        .select()
+	        .apis(RequestHandlerSelectors.any())
+	        .paths(PathSelectors.any())
+	        .build()
+	        .useDefaultResponseMessages(false)
+	        .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
+	}
+    
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public List<ErrorFormDTO> handle(MethodArgumentNotValidException exception) {
