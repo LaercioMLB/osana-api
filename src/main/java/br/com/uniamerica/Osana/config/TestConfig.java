8959d31aa9627a93c94dc948e754d3eb9b9696b1
Laércio Bubiak <49946934+LaercioMLB@users.noreply.github.com>
2022-09-16 11:01:13 -0300

    Revert "Status (#12)"
    This reverts commit 90a29c137f596e73a7387a78a4724dbc5398fd15.

---
+++ src/main/java/br/com/uniamerica/Osana/config/TestConfig.java
@@ -0,0 +1,27 @@
+package br.com.uniamerica.osanabackend.config;
+
+import br.com.uniamerica.osanabackend.entities.Usuario;
+import br.com.uniamerica.osanabackend.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1,u2));
+    }
+}
--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO USUARIO(name, username, email, password) VALUES('Desenvolvedor Account', 'desenv gestor', 'desenv@email.com', '$2a$10$pT82fBlCD2ydMShY/jqjpOFBlqMZlHmDRXJkYMVBI7HZmyGwTd1yu');
-INSERT INTO USUARIO(name, username, email, password) VALUES('Desenvolvedor Account', 'desenv tecnico', 'desenv@email.com', '$2a$10$pT82fBlCD2ydMShY/jqjpOFBlqMZlHmDRXJkYMVBI7HZmyGwTd1yu');
-
-INSERT INTO ROLE(name) VALUES('ROLE_GESTOR');
-INSERT INTO ROLE(name) VALUES('ROLE_TECNICO');
-
-INSERT INTO USUARIO_ROLES(usuario_id, roles_id) VALUES(1,1);
-INSERT INTO USUARIO_ROLES(usuario_id, roles_id) VALUES(2,2);
