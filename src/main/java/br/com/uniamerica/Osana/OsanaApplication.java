81a8f1c3da0e882f4c349fbb60ceefb20dcabd3a
LaercioMLB <lbubiak@hotmail.com>
2022-09-16 20:29:06 -0300

    :books: doc: add swagger

--- src/main/java/br/com/uniamerica/Osana/OsanaApplication.java
+++ src/main/java/br/com/uniamerica/Osana/OsanaApplication.java
@@ -3,9 +3,11 @@ package br.com.uniamerica.Osana;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class OsanaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OsanaApplication.class, args);
