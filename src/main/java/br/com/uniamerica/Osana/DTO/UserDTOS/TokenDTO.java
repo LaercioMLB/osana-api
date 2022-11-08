cd381c023c1837e7daea79a3d335430a8e791468
willian <willianthiagofozz@hotmail.com>
2022-11-08 19:31:51 -0300

    return user in login

--- src/main/java/br/com/uniamerica/Osana/DTO/UserDTOS/TokenDTO.java
+++ src/main/java/br/com/uniamerica/Osana/DTO/UserDTOS/TokenDTO.java
@@ -1,12 +1,16 @@
 package br.com.uniamerica.Osana.DTO.UserDTOS;
 
+import java.io.Serializable;
+
 public class TokenDTO {
     private String token;
     private String typeRequest;
+    private UsuarioDTO user;
 
-    public TokenDTO(String token, String typeRequest) {
+    public TokenDTO(String token, String typeRequest, UsuarioDTO user) {
         this.token = token;
         this.typeRequest = typeRequest;
+        this.user = user;
     }
 
     public String getToken() {
@@ -15,5 +19,9 @@ public class TokenDTO {
 
     public String getTypeRequest() {
         return typeRequest;
+    }
+
+    public UsuarioDTO getUser() {
+        return user;
     }
 }
